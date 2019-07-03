fcd31e0d8fafe9c5a3062f7c021d7bef2a623357
Janie <evgeniyaberseneva89@gmail.com>
2019-07-03 11:31:02 +0300

    added link to color box & color SP setup

--- src/App.js
+++ src/App.js
@@ -27,6 +27,11 @@ function App() {
           />
         )}
       />
+      <Route
+        exact
+        path="/palette/:paletteId/:colorId"
+        render={() => <h1>Single Color Page</h1>}
+      />
     </Switch>
 
     // <div>
