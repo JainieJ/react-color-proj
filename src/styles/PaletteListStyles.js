56d169567f1b359e2c5621fa7fe7de9538bf0def
Janie <evgeniyaberseneva89@gmail.com>
2019-07-07 17:36:30 +0300

    add bg SVG to PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,21 +1,28 @@
 import sizes from "./sizes";
+import bg from "./bg.svg";
 
 export default {
   root: {
-    backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
-    justifyContent: "center"
+    justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    backgroundColor: "#141caa",
+    backgroundImage: `url(${bg})`,
+    overflow: "scroll"
   },
   container: {
-    width: "50%",
+    width: "60%",
     display: "flex",
-    alignItems: "flex-start",
+    alignItems: "center",
     flexDirection: "column",
     flexWrap: "wrap",
     [sizes.down("lg")]: {
       width: "80%"
+    },
+    [sizes.down("md")]: {
+      width: "65%"
     },
     [sizes.down("xs")]: {
       width: "60%"
@@ -28,7 +35,10 @@ export default {
     alignItems: "center",
     color: "white",
     "& a": {
-      color: "white"
+      color: "white",
+      [sizes.down("xs")]: {
+        textAlign: "right"
+      }
     }
   },
   palettes: {
@@ -36,9 +46,10 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3,30%)",
-    gridGap: "1.5rem",
+    gridGap: "5%",
     [sizes.down("md")]: {
-      gridTemplateColumns: "repeat(2,50%)"
+      gridTemplateColumns: "repeat(2,50%)",
+      gridGap: "0.5rem"
     },
     [sizes.down("xs")]: {
       gridTemplateColumns: "repeat(1,100%)"
